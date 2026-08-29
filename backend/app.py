ed3316cf190ddf8db8a8d799891a8f370f4dfceb
Saurabhchaudhary781212 <saurabhchaudhary4021@gmail.com>
2026-08-29 12:19:50 +0530

    fix backend cors

--- backend/app.py
+++ backend/app.py
@@ -1,137 +1,27 @@
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-
-# from routes.auth_routes import auth_bp
-# from routes.data_routes import data_bp
-
-
-# app = Flask(__name__)
-
-
-# # -----------------------------
-# # CORS CONFIGURATION
-# # -----------------------------
-
-# CORS(
-#     app,
-#     resources={
-#         r"/api/*": {
-#             "origins": [
-#                 "http://localhost:5173"
-#             ]
-#         }
-#     },
-#     methods=[
-#         "GET",
-#         "POST",
-#         "PUT",
-#         "DELETE",
-#         "OPTIONS"
-#     ],
-#     allow_headers=[
-#         "Content-Type",
-#         "Authorization"
-#     ]
-# )
-
-
-# # -----------------------------
-# # BLUEPRINTS
-# # -----------------------------
-
-# # Authentication APIs
-# app.register_blueprint(
-#     auth_bp,
-#     url_prefix="/api/auth"
-# )
-
-
-# # Data upload + analysis APIs
-# app.register_blueprint(
-#     data_bp,
-#     url_prefix="/api/data"
-# )
-
-
-# # -----------------------------
-# # HOME ROUTE
-# # -----------------------------
-
-# @app.route("/")
-# def home():
-#     return jsonify({
-#         "success": True,
-#         "message": "Order Analytics API is running"
-#     })
-
-
-# # -----------------------------
-# # RUN SERVER
-# # -----------------------------
-
-# if __name__ == "__main__":
-#     app.run(
-#         host="127.0.0.1",
-#         port=5000,
-#         debug=True
-#     )
-
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-
 from routes.auth_routes import auth_bp
-from routes.data_routes import data_bp
-
 
 app = Flask(__name__)
 
-
 CORS(
     app,
-    resources={
-        r"/api/*": {
-            "origins": [
-                order-analysis-one.vercel.app
-            ]
-        }
-    },
-    methods=[
-        "GET",
-        "POST",
-        "PUT",
-        "DELETE",
-        "OPTIONS"
+    origins=[
+        "https://order-analysis-one.vercel.app"
     ],
-    allow_headers=[
-        "Content-Type",
-        "Authorization"
-    ]
+    supports_credentials=True
 )
 
-
-app.register_blueprint(
-    auth_bp,
-    url_prefix="/api/auth"
-)
-
-
-app.register_blueprint(
-    data_bp,
-    url_prefix="/api/data"
-)
+app.register_blueprint(auth_bp)
 
 
 @app.route("/")
 def home():
-    return jsonify({
+    return {
         "success": True,
         "message": "Order Analytics API is running"
-    })
+    }
 
 
 if __name__ == "__main__":
-    app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000)
